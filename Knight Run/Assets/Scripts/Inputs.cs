9aeb8431712b645ffbcc17e10e4a458a35180d0d
Gavin <goldenwarrior97@gmail.com>
2017-01-23 02:48:48 +0800

    Began Menu Coding
    So far:
    - Start Screen Implemented
    - Menu Screen Implemented

---
+++ Knight Run/Assets/Scripts/Inputs.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour {
+    #region Keyboard Inputs
+    public bool LMB;
+    public bool keyW;
+    public bool keyS;
+    public bool keyArrowUp;
+    public bool keyArrowDown;
+    public bool keyEnter;
+    public bool escKey;
+    public bool keySpace;
+    #endregion
+    #region Keyboard Inputs Initialization
+    public string selectK;
+    public string selectJ;
+    public string wKey;
+    public string sKey;
+    public string arrowKeyDown;
+    public string arrowKeyUp;
+    public string enterKey;
+    public string escKeyK;
+    public string spaceKey;
+    #endregion
+
+    private void Awake() {
+
+    }
+    // Use this for initialization
+    void Start () {
+        selectK = "SelectK";
+        selectJ = "SelectJ";
+        wKey = "wKey";
+        sKey = "sKey";
+        arrowKeyDown = "downArrow";
+        arrowKeyUp = "upArrow";
+        enterKey = "enterKey";
+        escKeyK = "escKeyK";
+        spaceKey = "spaceKey";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        HandleKeyboardInput();
+        MenuLogic();
+	}
+
+    private void HandleKeyboardInput() {
+        LMB = Input.GetButtonDown(selectK);
+        keyW = Input.GetButtonDown(wKey);
+        keyS = Input.GetButtonDown(sKey);
+        keyArrowDown = Input.GetButtonDown(arrowKeyDown);
+        keyArrowUp = Input.GetButtonDown(arrowKeyUp);
+        keyEnter = Input.GetButtonDown(enterKey);
+        escKey = Input.GetButtonDown(escKeyK);
+        keySpace = Input.GetButtonDown(spaceKey);
+    }
+
+    public void MenuLogic() {
+
+    }
+}
---
+++ Knight Run/Assets/Scripts/Menu.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public string currentState;
+    public Inputs inputs;
+
+    [System.Serializable]
+    public class StartScreen {
+        public GameObject StartCanvas;
+        public GameObject PressToStart;
+    }
+    public StartScreen startScreen;
+
+    [System.Serializable]
+    public class MenuScreen {
+        public GameObject MenuCanvas;
+        public GameObject MOStory, MOTraining, MOSettings, MOQuit;
+        public GameObject SelectStory, SelectTraining, SelectSettings, SelectQuit;
+    }
+    public MenuScreen menuScreen;
+
+	// Use this for initialization
+	void Start () {
+        #region StartScreen
+        startScreen.StartCanvas.SetActive(true);
+        startScreen.PressToStart.SetActive(true);
+        #endregion
+        #region MenuScreen
+        menuScreen.MenuCanvas.SetActive(false);
+        menuScreen.MOStory.SetActive(false);
+        menuScreen.MOTraining.SetActive(false);
+        menuScreen.MOSettings.SetActive(false);
+        menuScreen.MOQuit.SetActive(false);
+        menuScreen.SelectStory.SetActive(false);
+        menuScreen.SelectTraining.SetActive(false);
+        menuScreen.SelectSettings.SetActive(false);
+        menuScreen.SelectQuit.SetActive(false);
+        #endregion
+    }
+	
+    public void pressToStart() {
+        startScreen.StartCanvas.SetActive(false);
+        startScreen.PressToStart.SetActive(false);
+        menuScreen.MenuCanvas.SetActive(true);
+        menuScreen.MOStory.SetActive(false);
+        menuScreen.MOTraining.SetActive(true);
+        menuScreen.MOSettings.SetActive(true);
+        menuScreen.MOQuit.SetActive(true);
+        menuScreen.SelectStory.SetActive(true);
+        menuScreen.SelectTraining.SetActive(false);
+        menuScreen.SelectSettings.SetActive(false);
+        menuScreen.SelectQuit.SetActive(false);
+    }
+
+    public void MOStory() {
+        menuScreen.MOStory.SetActive(false);
+        menuScreen.MOTraining.SetActive(true);
+        menuScreen.MOSettings.SetActive(true);
+        menuScreen.MOQuit.SetActive(true);
+        menuScreen.SelectStory.SetActive(true);
+        menuScreen.SelectTraining.SetActive(false);
+        menuScreen.SelectSettings.SetActive(false);
+        menuScreen.SelectQuit.SetActive(false);
+    }
+    public void MOTraining() {
+        menuScreen.MOStory.SetActive(true);
+        menuScreen.MOTraining.SetActive(false);
+        menuScreen.MOSettings.SetActive(true);
+        menuScreen.MOQuit.SetActive(true);
+        menuScreen.SelectStory.SetActive(false);
+        menuScreen.SelectTraining.SetActive(true);
+        menuScreen.SelectSettings.SetActive(false);
+        menuScreen.SelectQuit.SetActive(false);
+    }
+    public void MOSettings() {
+        menuScreen.MOStory.SetActive(true);
+        menuScreen.MOTraining.SetActive(true);
+        menuScreen.MOSettings.SetActive(false);
+        menuScreen.MOQuit.SetActive(true);
+        menuScreen.SelectStory.SetActive(false);
+        menuScreen.SelectTraining.SetActive(false);
+        menuScreen.SelectSettings.SetActive(true);
+        menuScreen.SelectQuit.SetActive(false);
+    }
+    public void MOQuit() {
+        menuScreen.MOStory.SetActive(true);
+        menuScreen.MOTraining.SetActive(true);
+        menuScreen.MOSettings.SetActive(true);
+        menuScreen.MOQuit.SetActive(false);
+        menuScreen.SelectStory.SetActive(false);
+        menuScreen.SelectTraining.SetActive(false);
+        menuScreen.SelectSettings.SetActive(false);
+        menuScreen.SelectQuit.SetActive(true);
+    }
+}
