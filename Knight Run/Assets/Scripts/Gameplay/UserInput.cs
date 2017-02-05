d4ffd728c12d10364b9cbadb0502c38936c64d72
Gavin <goldenwarrior97@gmail.com>
2017-02-05 20:04:05 +0800

    Major Update
    - Character Controller is inside (Basic)
    - Camera Works (Basic)
    - Animations play when button is pressed. Will improve to split it and
    implement legit combo system with programmer’s help.

---
+++ Knight Run/Assets/Scripts/Gameplay/UserInput.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour {
+
+	CharacterMovement characterMove;
+
+	[System.Serializable]
+	public class InputSettings {
+		public string verticalAxis = "Vertical";
+		public string horizontalAxis = "Horizontal";
+		public string jumpButton = "Jump";
+		public string attackButton1 = "Attack1";
+		public string attackButton2 = "Attack2";
+	}
+	[SerializeField]
+	public InputSettings input;
+
+	[System.Serializable]
+	public class OtherSettings {
+		public float lookSpeed = 5.0f;
+		public float lookDistance = 10.0f;
+		public bool requireInputForTurn = true;
+	}
+	[SerializeField]
+	public OtherSettings other;
+
+	Camera mainCam;
+	// Use this for initialization
+	void Start () {
+		characterMove = GetComponent<CharacterMovement> ();
+		mainCam = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (characterMove) {
+			characterMove.Animate (Input.GetAxis ("Vertical"), Input.GetAxis ("Horizontal"));
+
+			if (Input.GetButtonDown ("Jump")) {
+				//characterMove.Jump();
+			}
+			if (Input.GetButtonDown ("Attack1")) {
+				characterMove.Attack1();
+			}
+			if (Input.GetButtonDown ("Attack2")) {
+				characterMove.Attack2();
+			}
+		}
+		if (mainCam) {
+			if (other.requireInputForTurn) {
+				if (Input.GetAxis (input.horizontalAxis) != 0 || Input.GetAxis (input.verticalAxis) != 0) {
+					CharacterLook ();
+				} else {
+					CharacterLook ();
+				}
+			}
+		}
+
+	}
+	//make the character look at a forward point from the camera
+	void CharacterLook() {
+		Transform mainCamT = mainCam.transform;
+		Transform pivotT = mainCamT.parent;
+		Vector3 pivotPos = pivotT.position;
+		Vector3 lookTarget = pivotPos + (pivotT.forward * other.lookDistance);
+		Vector3 thisPos = transform.position;
+		Vector3 lookDir = (lookTarget - thisPos);
+		Quaternion lookRot = Quaternion.LookRotation (lookDir);
+		lookRot.x = 0;
+		lookRot.z = 0;
+
+		Quaternion newRotation = Quaternion.Lerp (transform.rotation, lookRot, Time.deltaTime * other.lookSpeed);
+		transform.rotation = newRotation;
+	}
+}
