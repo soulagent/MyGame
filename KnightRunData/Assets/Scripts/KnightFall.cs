547d070dccd29d2751f7fbed782e145666a6d5f4
Gavin <goldenwarrior97@gmail.com>
2017-06-20 00:28:53 +0800

    Marketing Additions
    Revamped marketing assets to include legal things at the start of the game.
    
    - Added 2 scripts that can kill and spawn any single gameobject. (Will work to refine it in the future, for use in the actual game.)

---
+++ KnightRunData/Assets/Scripts/KnightFall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightFall : MonoBehaviour {
+
+    public float KillTimer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(InitiateKnightFall());
+    }
+
+    IEnumerator InitiateKnightFall() {
+        yield return new WaitForSeconds(KillTimer);
+        DestroyObject(this.gameObject);
+    }
+}
---
+++ KnightRunData/Assets/Scripts/KnightRise.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightRise : MonoBehaviour {
+    /*
+    [System.Serializable]
+    public struct ToSpawn {
+        public Spawn[] spawn;
+    }
+    
+    [System.Serializable]
+    public struct Spawn {
+        public KnightRise[] GameObject;
+    }
+
+    [Header("To Spawn")]
+    public float SpawnTimer;
+    public KnightRise[] ToRise = new KnightRise[0];
+    */
+
+    [Header("To Spawn")]
+    public float SpawnTimer;
+    public GameObject toSpawn;
+
+    void Update () {
+        StartCoroutine(InitiateKnightRise());
+    }
+
+    IEnumerator InitiateKnightRise() {
+        yield return new WaitForSeconds(SpawnTimer);
+        toSpawn.SetActive(true);
+        Destroy(this);
+    }
+}
